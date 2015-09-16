fbdb56c619daf66bb675b8a97ec9073f64d34a3d
mfiosbm <mfiosbm@us.ibm.com>
2015-09-17 01:15:46 +0530

    Helper function to be used in bootprint-swagger such that referenced parameters can be displayed as default parameters.

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -8,9 +8,14 @@ module.exports = {
      */
     "json-schema--subschema-name": function(url) {
         return url.replace("#/definitions/","");
-    }
-};
+    },
 
+    "json-schema--parameter-name": function(url) {
+        return url.replace('#/parameters/', '');
+    },
+
+    "json-schema--parameter-properties": parameterRef
+};
 
 /**
  * Returns a descriptive string for a datatype
@@ -37,4 +42,17 @@ function dataType(value) {
     return value.type;
 }
 
-
+/* Returns a parameter value for a parameter key
+ * @param parameters 
+ * @param parameterName
+ * @param parameterKey
+ * @returns parameter value
+ */
+function parameterRef(parameters, parameterName, key) {
+    var parametersObj = parameters.hash.parameters;
+    var paramName = parameters.hash.parameterName.replace('#/parameters/','');
+    var objectKey = parameters.hash.key;
+    if (objectKey == 'enum')
+        console.log(parametersObj[paramName]);
+    return parametersObj[paramName][objectKey];
+}
